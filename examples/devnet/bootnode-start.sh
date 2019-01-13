82839e235f5ab237f297e8eb53df80b2a5488b3c
Jason Carter <jsyqrt@gmail.com>
2019-01-13 15:00:11 +0800

    added compiled bootnode

--- examples/devnet/bootnode-start.sh
+++ examples/devnet/bootnode-start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-bootnode='../../build/bin/bootnode'
-nodekey='bootnode/boot.key'
+bootnode='./bootnode'
+nodekey='bootkey/boot.key'
 addr='0.0.0.0:30310'
 
 nohup $bootnode -nodekey $nodekey -verbosity 9 -addr $addr 2>&1 >> logs/bootnode.log &
